82fa07f3ed7b2664cd50b754ec0fa8958499d4ca
mikunurtilek <pokllkop031@gmail.com>
2024-09-22 12:02:34 +0500

    finished task

---
+++ nurtilek.py
@@ -0,0 +1,13 @@
+def sum_even_numbers(a, b):
+    if a > b:
+        return f"Ошибка: a должно быть меньше или равным b"
+
+    total_sum = 0
+    for num in range(a, b + 1):
+        if num % 2 == 0:
+            total_sum += num
+
+    return total_sum
+
+
+print(sum_even_numbers(1, 5))
